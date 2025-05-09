707e9f008378d644a812ea9ce2a4467865ad4e96
NotKieranF <89042089+NotKieranF@users.noreply.github.com>
2025-05-09 16:39:53 -0400

    Updated call65, cleaned up
    * Updated call65 to latest version
    * Cleaned up old header files and cfg file

--- src/includes/debug_state.h
+++
@@ -1,5 +0,0 @@
-.IFNDEF	DEBUG_STATE_H
-
-.GLOBAL		debug_state, init_debug_state
-
-.ENDIF
--- src/includes/fine_attributes.h
+++
@@ -1,7 +0,0 @@
-.IFNDEF	FINE_ATTRIBUTES_H
-FINE_ATTRIBUTES_H = 1
-
-.GLOBAL		fine_attributes_kernal
-.GLOBALZP	y_scroll_ptr, t_lo_ptr, fine_x_ptr, scanline_accum
-
-.ENDIF
